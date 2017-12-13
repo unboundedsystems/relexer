d3cda00dacc5cbe1bd8744cb53227114cc4b576c
Manish Vachharajani <manishv@unbounded.systems>
2017-12-13 13:06:16 -0700

    More tests and a refactoring into a Lexer class to make things sane.

--- test/relexer.spec.ts
+++ test/relexer.spec.ts
@@ -2,6 +2,12 @@ import * as relexer from "..";
 import * as sb from "stream-buffers";
 import * as util from 'util';
 
+type Token = { text:string, pos: number };
+async function pushToken(tokens: Token[], match: string, pos: number): Promise<void> {
+    tokens.push({ text: match, pos: pos });
+}
+async function ignore(): Promise<void> { }
+
 describe('Single Rule', () => {
     let act: (match: string, pos: number) => Promise<void>;
 
@@ -36,7 +42,7 @@ describe('Single Rule', () => {
         });
 
         try {
-            await lexer(input);
+            await lexer.lex(input);
         } catch (e) {
             e.should.fail();
         }
@@ -44,11 +50,76 @@ describe('Single Rule', () => {
     });
 });
 
+describe('Multiple Easy Rules', () => {
+    let tokens: { text: string, pos: number }[] = [];
+    const lpushToken = (m: string, p: number) => pushToken(tokens, m, p);
+
+    async function ignore(): Promise<void> { }
+    const rules: relexer.Rules = [
+        { re: 'abc', action: lpushToken },
+        { re: 'def', action: lpushToken },
+        { re: 'ghi', action: lpushToken },
+        { re: '\\s+', action: ignore }
+    ];
+
+    let lexer: relexer.Lexer;
+
+    beforeEach(() => {
+        tokens = [];
+        lexer = relexer.create(rules);
+    });
+
+    it('should match tokens', async () => {
+        const ins = new sb.ReadableStreamBuffer();
+        process.nextTick(() => {
+            ins.put("abc def ghi")
+            ins.stop();
+        });
+        await lexer.lex(ins);
+        const matches = tokens.map((val) => val.text);
+        const positions = tokens.map((val) => val.pos);
+        matches.should.be.eql(['abc', 'def', 'ghi']);
+        positions.should.be.eql([0, 4, 8]);
+    })
+});
+
+describe('Multiple Overlapping Rules', () => {
+    let tokens: Token[] = [];
+    const lpushToken = (m: string, p: number) => pushToken(tokens, m, p);
+    const rules: relexer.Rules = [
+        { re: 'abc', action: lpushToken },
+        { re: 'abcdef', action: lpushToken },
+        { re: 'defghi', action: lpushToken },
+        { re: 'def', action: lpushToken },
+        { re: '\\s+', action: ignore }
+    ];
+
+    let lexer: relexer.Lexer;
+    beforeEach(() => {
+        tokens = [];
+        lexer = relexer.create(rules);
+    });
+
+    it('should match shorter strings that come first, first', async () => {
+        const ins = new sb.ReadableStreamBuffer();
+        process.nextTick(() => {
+            ins.put("abc abcdef defghi def")
+            ins.stop();
+        });
+        await lexer.lex(ins);
+        const matches = tokens.map((val) => val.text);
+        const positions = tokens.map((val) => val.pos);
+        matches.should.be.eql(['abc', 'abc', 'def', 'defghi', 'def']);
+        positions.should.be.eql([0, 4, 7, 11, 18]);
+
+    });
+})
+
 describe('Rule Checks', () => {
     it('forbids capturing parentheses in rules', () => {
         const ruleset: relexer.Rules = [
             { re: 'xx(abc)', action: async (match, pos) => { return; } }
-        ]
+        ];
         try {
             relexer.create(ruleset);
         } catch (e) {
@@ -60,11 +131,23 @@ describe('Rule Checks', () => {
     it('allows non-capturing parentheses in rules', () => {
         const ruleset: relexer.Rules = [
             { re: '(?:abc)', action: async (match, pos) => { ; } }
-        ]
+        ];
         try {
             relexer.create(ruleset);
         } catch (e) {
             e.should.fail();
+        }
+    });
+
+    it('requires actions to be functions', () => {
+        const ruleset: relexer.Rules = [
+            { re: '(?:abc)', action: <relexer.Action>({}) }
+        ]
+        try {
+            relexer.create(ruleset);
+        } catch (e) {
+            e.should.be.Error();
+            e.message.should.containEql("Action is not a function");
         }
     });
 });
