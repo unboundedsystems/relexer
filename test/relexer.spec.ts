63776f042e69b75f5f7284b0d536aea5c5d82db8
Manish Vachharajani <manishv@unbounded.systems>
2017-12-14 13:43:57 -0700

    Add chunked tests, fix bugs, and and non-chunked tests to cover a few missing cases.

--- test/relexer.spec.ts
+++ test/relexer.spec.ts
@@ -1,12 +1,20 @@
 import * as relexer from "..";
 import * as sb from "stream-buffers";
 import * as util from 'util';
+import { read } from "fs";
 
-type Token = { text:string, pos: number };
+type Token = { text: string, pos: number };
 async function pushToken(tokens: Token[], match: string, pos: number): Promise<void> {
     tokens.push({ text: match, pos: pos });
 }
 async function ignore(): Promise<void> { }
+
+function putOnTick(s: sb.ReadableStreamBuffer, txt: string) {
+    process.nextTick(() => {
+        s.put(txt);
+        s.stop();
+    });
+}
 
 describe('Single Rule', () => {
     let act: (match: string, pos: number) => Promise<void>;
@@ -36,10 +44,7 @@ describe('Single Rule', () => {
         };
 
         const input = new sb.ReadableStreamBuffer();
-        process.nextTick(() => {
-            input.put('abc');
-            input.stop();
-        });
+        putOnTick(input, 'abc');
 
         try {
             await lexer.lex(input);
@@ -71,16 +76,29 @@ describe('Multiple Easy Rules', () => {
 
     it('should match tokens', async () => {
         const ins = new sb.ReadableStreamBuffer();
-        process.nextTick(() => {
-            ins.put("abc def ghi")
-            ins.stop();
-        });
+        putOnTick(ins, 'abc def ghi');
         await lexer.lex(ins);
         const matches = tokens.map((val) => val.text);
         const positions = tokens.map((val) => val.pos);
         matches.should.be.eql(['abc', 'def', 'ghi']);
         positions.should.be.eql([0, 4, 8]);
     })
+
+    it('should not match with intervening character', async () => {
+        const ins = new sb.ReadableStreamBuffer();
+        putOnTick(ins, 'abc xdef');
+        try {
+            await lexer.lex(ins);
+            throw new Error("Whoops");
+        } catch (e) {
+            tokens.should.eql([{ text: 'abc', pos: 0 }]);
+            e.should.be.instanceOf(relexer.LexError);
+            e.message.should.containEql("No rule matched");
+            e.start.should.equal(4);
+            e.end.should.equal(5);
+            e.text.should.equal("x");
+        }
+    });
 });
 
 describe('Multiple Overlapping Rules', () => {
@@ -102,10 +120,8 @@ describe('Multiple Overlapping Rules', () => {
 
     it('should match shorter strings that come first, first', async () => {
         const ins = new sb.ReadableStreamBuffer();
-        process.nextTick(() => {
-            ins.put("abc abcdef defghi def")
-            ins.stop();
-        });
+        putOnTick(ins, 'abc abcdef defghi def');
+
         await lexer.lex(ins);
         const matches = tokens.map((val) => val.text);
         const positions = tokens.map((val) => val.pos);
@@ -148,6 +164,76 @@ describe('Rule Checks', () => {
         } catch (e) {
             e.should.be.Error();
             e.message.should.containEql("Action is not a function");
+        }
+    });
+});
+
+async function nextTick() {
+    return new Promise<void>((res, rej) => {
+        process.nextTick(() => res());
+    })
+}
+
+describe('Chunked Stream', () => {
+    let tokens: Token[] = [];
+    const lpushToken = (m: string, p: number) => pushToken(tokens, m, p);
+    const rules: relexer.Rules = [
+        { re: 'abc', action: lpushToken },
+        { re: '0123456789', action: lpushToken },
+        { re: '\ue05a\ue05a', action: lpushToken },
+        { re: '\\s+', action: ignore }
+    ];
+
+    let lexer: relexer.Lexer;
+    beforeEach(() => {
+        tokens = [];
+        lexer = relexer.create(rules);
+    });
+
+    async function checkToks(input: string, refToks: string[], refPos: number[],
+        chunkSize: number, lexOpts?: relexer.LexOptions) {
+
+        const s = new sb.ReadableStreamBuffer({
+            chunkSize: chunkSize
+        });
+        const lexp = lexer.lex(s, lexOpts);
+        putOnTick(s, input);
+        await lexp;
+
+        const matches = tokens.map((val) => val.text);
+        const positions = tokens.map((val) => val.pos);
+        matches.should.be.eql(refToks);
+        positions.should.a.eql(refPos);
+    }
+
+    it('should match "abc abc"', async () => {
+        await checkToks('abc abc', ['abc', 'abc'], [0, 4], 2)
+    });
+
+    it('should match double \ue05a', async () => {
+        //This tests if utf-8 partial character decoding is working right.
+        await checkToks('abc \ue05a\ue05a 0123456789',
+            ['abc', '\ue05a\ue05a', '0123456789'],
+            [0, 4, 7],
+            1
+        );
+    });
+
+    it('should not match 0123456789 because it is too long', async () => {
+        const s = new sb.ReadableStreamBuffer({ chunkSize: 1 });
+        const lexp = lexer.lex(s, { aggregateUntil: 9 });
+        putOnTick(s, 'abc 0123456789');
+
+        try {
+            await lexp;
+            throw new Error("Whoops");
+        } catch (e) {
+            tokens.should.be.eql([{ text: 'abc', pos: 0 }]);
+            e.should.be.instanceOf(relexer.LexError);
+            e.message.should.containEql("No rule matched");
+            e.start.should.equal(4);
+            e.end.should.equal(13);
+            e.text.should.equal("012345678");
         }
     });
 });
